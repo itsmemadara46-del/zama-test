d04263fc8d934e44ba6cecc6215239918832717b
itsmemadara46-del <itsmemadara46@gmail.com>
2026-01-13 19:41:58 +0530

    Update frontend.js

--- public/frontend.js
+++ public/frontend.js
@@ -75,6 +75,7 @@ function renderEvents() {
   pageData.forEach(e => {
     // TABLE
     const tr = document.createElement("tr");
+    if (e.og) tr.classList.add("og-row"):
     tr.innerHTML = `
       <td>Verified</td>
       <td>
